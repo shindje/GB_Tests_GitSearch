b14ec818b346340d7405d64d8c798955d1d15360
shindje <aleks.shindje@gmail.com>
2021-06-21 11:34:04 +0300

    Lesson 5: UI Automator

--- app/src/androidTestReal/java/com.geekbrains.tests/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTestReal/java/com.geekbrains.tests/espresso/MainActivityEspressoTest.kt
@@ -33,7 +33,7 @@ class MainActivityEspressoTest {
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
 
         onView(isRoot()).perform(delay())
-        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2424")))
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2430")))
     }
 
     private fun delay(): ViewAction? {
