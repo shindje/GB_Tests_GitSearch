94767f506d19395e68ec4bca82652992901e7791
shindje <aleks.shindje@gmail.com>
2021-06-21 12:20:27 +0300

    Task 2: DetailsScreenTest - search result count

--- app/src/androidTest/java/com/geekbrains/tests/automator/BehaviorTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/BehaviorTest.kt
@@ -3,9 +3,6 @@ package com.geekbrains.tests.automator
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
@@ -13,7 +10,6 @@ import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObject2
 import androidx.test.uiautomator.Until
-import com.geekbrains.tests.R
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -67,14 +63,14 @@ class BehaviorTest {
         //Устанавливаем значение
         editText.text = "UiAutomator"
         //Находим кнопку поиска
-        val toDetails: UiObject2 = uiDevice.findObject(
+        val searchBtn: UiObject2 = uiDevice.findObject(
             By.res(
                 packageName,
                 "searchButton"
             )
         )
         //Кликаем по ней
-        toDetails.click()
+        searchBtn.click()
 
         //Ожидаем конкретного события: появления текстового поля totalCountTextView.
         //Это будет означать, что сервер вернул ответ с какими-то данными, то есть запрос отработал.
---
+++ app/src/androidTest/java/com/geekbrains/tests/automator/DetailsScreenTest.kt
@@ -0,0 +1,70 @@
+package com.geekbrains.tests.automator
+
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.Until
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class DetailsScreenTest {
+    private val uiDevice: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val packageName = context.packageName
+
+    @Before
+    fun setup() {
+        uiDevice.pressHome()
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        context.startActivity(intent)
+        uiDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), TIMEOUT)
+    }
+
+    @Test
+    fun test_DetailsScreen_CorrectSearchResultCount() {
+        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
+        editText.text = "algol"
+        val searchBtn: UiObject2 = uiDevice.findObject(
+            By.res(
+                packageName,
+                "searchButton"
+            )
+        )
+        searchBtn.click()
+
+        uiDevice.wait(
+            Until.findObject(By.res(packageName, "totalCountTextView")), TIMEOUT
+        )
+
+        val toDetails: UiObject2 = uiDevice.findObject(
+            By.res(
+                packageName,
+                "toDetailsActivityButton"
+            )
+        )
+        toDetails.click()
+
+        val changedText =
+            uiDevice.wait(
+                Until.findObject(By.res(packageName, "totalCountTextView")),
+                TIMEOUT
+            )
+        Assert.assertEquals(changedText.text, "Number of results: $ALGOL_REPS_COUNT")
+    }
+
+    companion object {
+        private const val TIMEOUT = 5000L
+        private const val ALGOL_REPS_COUNT = 2431
+    }
+}
